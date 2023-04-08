c9587ac934cfa5eb291ab3b479d2d8734489da37
김규동 <kimgyudong@gimgyudong-ui-MacBookAir.local>
2023-04-09 00:18:10 +0900

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+    return (
+      <>
+        <h1>2020043872 김규동 입니다.</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </>
+    );
+  }
