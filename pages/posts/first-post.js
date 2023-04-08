5bdaba7b4ffcc3d81ce44d1879c4b0d1acd4e2ae
KIMGYUDONG <kimgyudong@gimgyudong-ui-MacBookAir.local>
2023-04-09 00:31:42 +0900

    second commit

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -4,6 +4,7 @@ export default function FirstPost() {
     return (
       <>
         <h1>2020043872 김규동 입니다.</h1>
+        <h1>복학생 입니다.</h1>
         <h2>
           <Link href="/">Back to home</Link>
         </h2>
